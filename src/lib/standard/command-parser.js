02954292235a5eaba13bb10b91b21de85934cc7a
Simon Zozol <simon.zozol@gmail.com>
2016-12-13 15:50:33 +0100

    command-parser passes first batch of test

--- src/lib/standard/command-parser.js
+++ src/lib/standard/command-parser.js
@@ -4,18 +4,31 @@ const eol = P.char('\n');
 
 
 // accept a string of control characters
-function blank(){return P.charIn(" \n\t").optrep()}
+function blank(){return P.charIn(" \n\t").optrep().map(blank=>'')}
 
 // accept a non-void string of character without control or special character
-function word() {return P.charNotIn(': \n\t').rep()}
+function word() {
+    return P.charNotIn(': \n\t').rep()
+    .map(array_of_char =>array_of_char.join('') )}
 
 //accept anything but a blank string
+//TODO: trim
 function text() {
-    return blank().then(P.any()).rep()
+    return blank()
+           .then(P.any().rep())
+        .map(a =>a[1].join('') )
+}
+
+function command(){
+    return P.string("image").or(P.string('warning')).or(P.string('info')).or(P.string('equation'))
 }
 
 function command_name() {
-    return blank().then(word()).then(blank())
+    return blank()
+           //.then(word())
+           .then(command())
+           .then(blank())
+           .map(a => a[0][1] )
 }
 
 function parameter() {
@@ -25,10 +38,10 @@ function parameter() {
 //accept this pattern:   !command:parameters
 function commandLine() {
     return blank()
-        .then(P.char('!'))
-        .then(command_name())
-        .then(P.char(':'))
-        .then(parameter())
+          .then(P.char('!'))
+          .then(command_name())
+          .then(P.char(':'))
+          .then(parameter())
 }
 
 export default class CommandParser {
